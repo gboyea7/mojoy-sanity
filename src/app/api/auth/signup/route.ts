751411e5af1f662c357cf0aa96fd66f00a9c443f
unknown <dtonye338@gmail.com>
2025-06-03 18:14:29 -1000

    fix

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -45,7 +45,7 @@ export async function POST(request: Request) {
     const transporter = nodemailer.createTransport({
       host: process.env.AWS_SES_SMTP_HOST,
       port: Number(process.env.AWS_SES_SMTP_PORT),
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.AWS_SES_SMTP_USER,
         pass: process.env.AWS_SES_SMTP_PASS,
